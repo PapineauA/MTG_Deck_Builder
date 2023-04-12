cf1f6d7f7c05739c03e4309f2cd14f2d604eb2b3
PapineauA <papineautaidan@gmail.com>
2023-04-11 23:20:26 -0400

    API works

--- Backend/app.py
+++ Backend/app.py
@@ -4,14 +4,3 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-
-@app.route('/cards')
-def get_cards():
-    conn = psycopg2.connect(dbname='cards')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM cards')
-    cards = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return jsonify(cards)
--- Backend/controllers/decks_controller.py
+++ Backend/controllers/decks_controller.py
@@ -1,12 +1,64 @@
 from flask import Flask, jsonify
 import psycopg2
 from flask_cors import CORS
+from mtgsdk import Card, Set
 
 app = Flask(__name__)
 CORS(app)
 
 
+def is_legal_in_commander(card):
+    print("checking if legal...")
+    return any(
+        legality.get('format') == 'Commander' for legality in card.legalities)
+
+
+def store_card_data(card):
+    print("storing card data...")
+
+    card_data = {
+        'name': card.name,
+        'manaCost': card.mana_cost,
+        'cmc': card.cmc,
+        'colors': card.colors,
+        'colorIdentity': card.color_identity,
+        'type': card.type,
+        'supertypes': card.supertypes,
+        'types': card.types,
+        'subtypes': card.subtypes,
+        'set': card.set,
+        'text': card.text,
+        'power': card.power,
+        'toughness': card.toughness,
+    }
+    return card_data
+
+
+def fetch_unique_commander_cards(limit):
+    print("fetching unique cards...")
+
+    unique_cards = {}
+    card_data_dict = {}
+    page = 1
+    while len(unique_cards) < limit:
+        cards = Card.where(page=page, pageSize=limit).all()
+        for card in cards:
+            if card.multiverse_id not in unique_cards and is_legal_in_commander(
+                    card):
+                unique_cards[card.multiverse_id] = card
+                card_data_dict[card.name] = store_card_data(card)
+                if len(unique_cards) == limit:
+                    break
+        page += 1
+
+    for index, card_data in enumerate(card_data_dict.values()):
+        print(f"{index + 1}. {card_data['name']} - {card_data['set']}")
+
+    return card_data_dict
+
+
 @app.route('/test')
 def get_test():
+    commanderCards = fetch_unique_commander_cards(500)
     data = {"message": "Hello from Flask!"}
     return jsonify(data)
